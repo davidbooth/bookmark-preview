0de69d08ae5abd67d9991b3bc6dfc181292c82ce
David Booth <david@booth.us.com>
2022-01-09 07:49:00 -0500

    refactored WSAScraper tests

--- classes/scraper/WSAScraper.test.tsx
+++ classes/scraper/WSAScraper.test.tsx
@@ -2,15 +2,34 @@
  * @jest-environment node
  */
 
+import axios from 'axios';
 import { WSAScraper, API_URL } from './WSAScraper';
-import mockedAxios from '../../mocks/axios';
+
+jest.mock('axios');
+const mockedGet = axios.get as jest.MockedFunction<typeof axios.get>;
 
 describe('WSAScraper', () => {
+    test('should return html and screenshot content', async () => {
+        const mockHTMLResponse = { data: '<html></html>' };
+        const mockScreenshotResponse = { data: { screenshot: 'BASE64' } };
+        mockedGet.mockResolvedValueOnce(mockHTMLResponse);
+        mockedGet.mockResolvedValueOnce(mockScreenshotResponse);
+        const expectedScreenshot = {
+            base64: `data:image/png;base64, ${mockScreenshotResponse.data.screenshot}`,
+        };
+
+        const scraper = new WSAScraper({ url: 'https://google.com' });
+        const { html, screenshot } = await scraper.getHTMLAndScreenshot();
+
+        expect(html).toEqual(mockHTMLResponse.data);
+        expect(screenshot).toEqual(expectedScreenshot);
+    });
+
     test('should return html content', async () => {
         const mockResponse = { data: '<html></html>' };
-        mockedAxios.get!.mockResolvedValueOnce(mockResponse);
+        mockedGet.mockResolvedValueOnce(mockResponse);
         const expectedApiParams = {
-            api_key: 'API-KEY',
+            api_key: undefined,
             device: undefined,
             render_js: 0,
             screenshot: 0,
@@ -23,15 +42,15 @@ describe('WSAScraper', () => {
         const scraper = new WSAScraper({ url: 'https://google.com' });
         const html = await scraper.getHTML();
 
-        expect(mockedAxios.get).toHaveBeenCalledWith(API_URL, { params: expectedApiParams });
+        expect(mockedGet).toHaveBeenCalledWith(API_URL, { params: expectedApiParams });
         expect(html).toEqual(mockResponse.data);
     });
 
     test('should return screenshot as base64 string', async () => {
         const mockResponse = { data: { screenshot: 'BASE64' } };
-        mockedAxios.get.mockResolvedValueOnce(mockResponse);
+        mockedGet.mockResolvedValueOnce(mockResponse);
         const expectedApiParams = {
-            api_key: 'API-KEY',
+            api_key: undefined,
             device: undefined,
             render_js: 1,
             screenshot: 1,
@@ -40,11 +59,14 @@ describe('WSAScraper', () => {
             wait_for: undefined,
             wait_until: undefined,
         };
+        const expectedResponse = {
+            base64: `data:image/png;base64, ${mockResponse.data.screenshot}`,
+        };
 
         const scraper = new WSAScraper({ url: 'https://google.com' });
         const response = await scraper.getScreenshot();
 
-        expect(mockedAxios.get).toHaveBeenCalledWith(API_URL, { params: expectedApiParams });
-        expect(response).toEqual(mockResponse.data);
+        expect(mockedGet).toHaveBeenCalledWith(API_URL, { params: expectedApiParams });
+        expect(response).toEqual(expectedResponse);
     });
 });
--- mocks/axios.tsx
+++
@@ -1,7 +0,0 @@
-jest.mock('axios');
-import axios from 'axios';
-
-const mockedAxios = axios as jest.MockedObject<typeof axios>;
-mockedAxios.get = jest.fn();
-
-export default mockedAxios;
